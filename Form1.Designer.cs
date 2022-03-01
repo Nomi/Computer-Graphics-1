1dc3def7b9e66d1ecd099178cf0d804b2b15637f
Noman <baba.noman@live.com>
2022-03-01 21:07:16 +0100

    Theme/Color changes. (Note: WinForms has bad color choices.)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,35 +53,42 @@ namespace Computer_Graphics_1
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Maroon;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.HighlightText;
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // mainTableLayoutPanel
             // 
+            this.mainTableLayoutPanel.BackColor = System.Drawing.Color.Black;
+            this.mainTableLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
             this.mainTableLayoutPanel.ColumnCount = 2;
             this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.mainTableLayoutPanel.Controls.Add(this.labsTabControl, 1, 0);
             this.mainTableLayoutPanel.Controls.Add(this.imagesTabControl, 0, 0);
             this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 28);
+            this.mainTableLayoutPanel.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 30);
             this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
             this.mainTableLayoutPanel.RowCount = 2;
             this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 0F));
-            this.mainTableLayoutPanel.Size = new System.Drawing.Size(800, 422);
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(800, 420);
             this.mainTableLayoutPanel.TabIndex = 1;
             // 
             // labsTabControl
@@ -92,19 +99,19 @@ namespace Computer_Graphics_1
             this.labsTabControl.Controls.Add(this.lab4TabPage);
             this.labsTabControl.Controls.Add(this.lab5TabPage);
             this.labsTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.labsTabControl.Location = new System.Drawing.Point(563, 3);
+            this.labsTabControl.Location = new System.Drawing.Point(562, 6);
             this.labsTabControl.Name = "labsTabControl";
             this.labsTabControl.SelectedIndex = 0;
-            this.labsTabControl.Size = new System.Drawing.Size(234, 416);
+            this.labsTabControl.Size = new System.Drawing.Size(232, 405);
             this.labsTabControl.TabIndex = 0;
             // 
             // lab1TabPage
             // 
-            this.lab1TabPage.BackColor = System.Drawing.Color.AliceBlue;
+            this.lab1TabPage.BackColor = System.Drawing.Color.DarkSlateGray;
             this.lab1TabPage.Location = new System.Drawing.Point(4, 25);
             this.lab1TabPage.Name = "lab1TabPage";
             this.lab1TabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.lab1TabPage.Size = new System.Drawing.Size(226, 387);
+            this.lab1TabPage.Size = new System.Drawing.Size(224, 376);
             this.lab1TabPage.TabIndex = 0;
             this.lab1TabPage.Text = "L1";
             // 
@@ -149,10 +156,10 @@ namespace Computer_Graphics_1
             // 
             this.imagesTabControl.Controls.Add(this.comparisontViewTabPage);
             this.imagesTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.imagesTabControl.Location = new System.Drawing.Point(3, 3);
+            this.imagesTabControl.Location = new System.Drawing.Point(6, 6);
             this.imagesTabControl.Name = "imagesTabControl";
             this.imagesTabControl.SelectedIndex = 0;
-            this.imagesTabControl.Size = new System.Drawing.Size(554, 416);
+            this.imagesTabControl.Size = new System.Drawing.Size(547, 405);
             this.imagesTabControl.TabIndex = 1;
             // 
             // comparisontViewTabPage
@@ -161,15 +168,14 @@ namespace Computer_Graphics_1
             this.comparisontViewTabPage.Location = new System.Drawing.Point(4, 25);
             this.comparisontViewTabPage.Name = "comparisontViewTabPage";
             this.comparisontViewTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.comparisontViewTabPage.Size = new System.Drawing.Size(546, 387);
+            this.comparisontViewTabPage.Size = new System.Drawing.Size(539, 376);
             this.comparisontViewTabPage.TabIndex = 0;
             this.comparisontViewTabPage.Text = "Comparison View";
             this.comparisontViewTabPage.UseVisualStyleBackColor = true;
             // 
             // comparisonTableLayout
             // 
-            this.comparisonTableLayout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.comparisonTableLayout.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.comparisonTableLayout.BackColor = System.Drawing.Color.Black;
             this.comparisonTableLayout.ColumnCount = 2;
             this.comparisonTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.comparisonTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0F));
@@ -184,7 +190,7 @@ namespace Computer_Graphics_1
             this.comparisonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
             this.comparisonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
             this.comparisonTableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45F));
-            this.comparisonTableLayout.Size = new System.Drawing.Size(540, 381);
+            this.comparisonTableLayout.Size = new System.Drawing.Size(533, 370);
             this.comparisonTableLayout.TabIndex = 0;
             // 
             // ogImgLabel
@@ -193,9 +199,9 @@ namespace Computer_Graphics_1
             this.ogImgLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.ogImgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ogImgLabel.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.ogImgLabel.Location = new System.Drawing.Point(4, 1);
+            this.ogImgLabel.Location = new System.Drawing.Point(3, 0);
             this.ogImgLabel.Name = "ogImgLabel";
-            this.ogImgLabel.Size = new System.Drawing.Size(531, 18);
+            this.ogImgLabel.Size = new System.Drawing.Size(527, 18);
             this.ogImgLabel.TabIndex = 1;
             this.ogImgLabel.Text = "Original Image:";
             this.ogImgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -206,9 +212,9 @@ namespace Computer_Graphics_1
             this.newImgLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.newImgLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.newImgLabel.ForeColor = System.Drawing.Color.LawnGreen;
-            this.newImgLabel.Location = new System.Drawing.Point(4, 190);
+            this.newImgLabel.Location = new System.Drawing.Point(3, 184);
             this.newImgLabel.Name = "newImgLabel";
-            this.newImgLabel.Size = new System.Drawing.Size(531, 18);
+            this.newImgLabel.Size = new System.Drawing.Size(527, 18);
             this.newImgLabel.TabIndex = 3;
             this.newImgLabel.Text = "After Processing:";
             this.newImgLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
